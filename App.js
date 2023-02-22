ba22006a7cc67dc58b39bcb7a82d1906d861f47b
LAPTOP-GSL4SKDE\rayid <rayidinavya@gmail.com>
2023-02-22 18:53:48 +0530

    adding components

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { View, Text } from "react-native";
+import MyComponent from './Func';
+import { Text, View,Style } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <MyComponent />
     </View>
   );
 }
---
+++ Func.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View ,Style} from 'react-native';
+export default function P(){
+    return (
+        <View>
+            <Text>
+           
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            hi hello this is navya
+            </Text>
+        </View>
+    )
+}
