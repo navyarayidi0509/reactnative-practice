d8ab88e3bf42dcfa25a1c37b0f38de1e334886bd
navyarayidi0509 <121232891+navyarayidi0509@users.noreply.github.com>
2023-02-23 10:03:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import MyComponent from './Func';
+import { Text, View,Style } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <MyComponent />
+    </View>
+  );
+}
---
+++ Classc.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View ,Style} from 'react-native';
+
+export default class MyComponent extends React.Component {
+   render() {
+      return (
+         <View>
+            <Text>Welcome to navya</Text>
+         </View>
+      );
+   }
+}
+//class component
