d75fa5474f8eaa6d19dee1da42d4c05fc894dad7
LAPTOP-GSL4SKDE\rayid <rayidinavya@gmail.com>
2023-02-24 17:46:53 +0530

    creating a list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import P from './Flex1'
+import P from './List1'
 export default class App extends React.Component {
    render() {
       return (
---
+++ List1.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+class P extends Component {
+   state = {
+      names: [
+        {name : 'navya' },
+        {name : 'sree' },
+      ]
+   }
+   alertItemName = (item) => {
+      alert(item.name)
+   }
+   render() {
+      return (
+         <View>
+            {
+               this.state.names.map((item) => (
+                  <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}
+                     onPress = {() => this.alertItemName(item)}>
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+               ))
+            }
+         </View>
+      )
+   }
+}
+export default P
+const styles = StyleSheet.create ({
+   container: {
+      padding: 10,
+      marginTop: 3,
+      backgroundColor: '#d9f9b1',
+      alignItems: 'center',
+   },
+   text: {
+      color:'blue'
+   }
+})
