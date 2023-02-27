cf18e437dc4102875f4ba157e380a2d6ea26d567
LAPTOP-GSL4SKDE\rayid <rayidinavya@gmail.com>
2023-02-27 17:46:33 +0530

    adding images examples

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import P from './Scrollview'
+import P from './Images'
 export default class App extends React.Component {
    render() {
       return (
---
+++ Images.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { View, Image } from 'react-native'
+
+const P = () => (
+   <Image source = {{uri:'http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.s2014.png'}}
+   style = {{ width: 200, height: 200 }}
+   />
+   
+)
+export default P
