e484ced18ed04326ef055f7fda770ede42372054
LAPTOP-GSL4SKDE\rayid <rayidinavya@gmail.com>
2023-02-28 13:11:56 +0530

    adding view examples

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import P from './Animation1'
+import P from './Viewex'
 export default class App extends React.Component {
    render() {
       return (
---
+++ Viewex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+class P extends Component{
+    render(){
+   return (
+      <View>
+         <View>
+            <Text>This is navya</Text>
+         </View>
+      </View>
+   )
+    }
+}
+export default P
